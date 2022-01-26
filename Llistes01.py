80d21d67f7bf1a256c4252e190efae382224c001
AdriaRoPOl <93370897+AdriaRoPOl@users.noreply.github.com>
2022-01-26 10:18:26 +0100

    Update Llistes01.py

--- Llistes01.py
+++ Llistes01.py
@@ -1,5 +1,6 @@
-word = input()
-if str(word) == str(word)[::-1] :
+print("Introdueix una paraula")
+paraula = input()
+if str(paraula) == str(paraula)[::-1] :
     print("Es capicua")
 else:
     print("No es capicua")
